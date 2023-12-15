d03ea94f31639d1d9e0f2fce8f11e6db68d53b15
Ilhama Novruzova <inovruzova07@gmail.com>
2023-12-15 17:02:52 +0400

    Designed the Mentor Info page and changed some elements in the data

--- scripts/info-script.js
+++ scripts/info-script.js
@@ -19,10 +19,42 @@ function renderMentorProfile(mentor) {
     document.getElementById('mentor-image').src = `assets/${mentor.image}`;
     document.getElementById('mentor-image').alt = mentor.name;
     document.getElementById('mentor-name').textContent = mentor.name;
-    document.getElementById('mentor-title').textContent = mentor.title;
-    document.getElementById('mentor-description').textContent = mentor.description;
+    document.getElementById('mentor-occupation').textContent = mentor.title;
+
+    function displayStarRating(container, rating) {
+        const fullStar = '\u2605';  // Unicode character for a solid star
+        const emptyStar = '\u2606'; // Unicode character for an outline star
+        let stars = '';
+    
+        // Add full stars
+        for (let i = 0; i < Math.floor(rating); i++) {
+            stars += fullStar;
+        }
+    
+        // Add half star if needed
+        if (rating % 1 !== 0) {
+            stars += '\u00BD' + emptyStar; // Unicode character for half star
+        }
+    
+        // Add empty stars
+        for (let i = Math.ceil(rating); i < 5; i++) {
+            stars += emptyStar;
+        }
+    
+        // Set the innerHTML of the container
+        container.innerHTML = `<span class="star-rating">${stars}</span>`;
+    }
+    
+    // Example usage
+    const ratingDiv = document.getElementById("mentor-rating");
+    displayStarRating(ratingDiv, mentor.rating);
+
+    document.getElementById('mentor-workplace').textContent = mentor.workplace;
+    document.getElementById('mentor-location').textContent = mentor.location;
+    document.getElementById('mentor-about').textContent = mentor.description;
     document.getElementById('mentor-linkedin').href = mentor.linkedin;
     document.getElementById('mentor-github').href = mentor.github;
+    document.getElementById('book-appointment').textContent = `Book for ${mentor.price}`;
 
     // Add event listener for booking appointment
     document.getElementById('book-appointment').addEventListener('click', () => {
