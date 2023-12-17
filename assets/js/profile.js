962684703973301355fcc7694ff829e471120fcc
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-12-17 23:29:18 +0400

    Fix problems.

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -194,3 +194,23 @@ function createJobCard(job) {
 
     return card;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var dropdownBtn = document.querySelector(".dropdown-toggle");
+    var dropdownMenu = document.querySelector(".dropdown-menu");
+  
+    dropdownBtn.addEventListener("click", function () {
+      console.log("Dropdown button clicked");
+      dropdownMenu.classList.toggle("active");
+    });
+  
+    window.addEventListener("click", function (event) {
+      if (!event.target.matches(".dropdown-toggle")) {
+        if (dropdownMenu.classList.contains("active")) {
+          console.log("Clicked outside dropdown; closing dropdown");
+          dropdownMenu.classList.remove("active");
+        }
+      }
+    });
+  });
+  
