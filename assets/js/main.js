47aa4a1f9924c85b23195e806f3f4fa78f588124
Ilhama Novruzova <inovruzova07@gmail.com>
2023-12-17 22:44:35 +0400

    Natavan's Final Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   var dropdownBtn = document.querySelector(".dropdown-toggle");
   var dropdownMenu = document.querySelector(".dropdown-menu");
 
-  dropdownBtn.addEventListener("click", function () {
-    console.log("Dropdown button clicked");
-    dropdownMenu.classList.toggle("active");
+  dropdownBtn.addEventListener("click", function (event) {
+      event.stopPropagation(); // Prevent the window click event from immediately closing the dropdown
+      dropdownMenu.classList.toggle("active");
   });
 
-  window.addEventListener("click", function (event) {
-    if (!event.target.matches(".dropdown-toggle")) {
+  window.addEventListener("click", function () {
       if (dropdownMenu.classList.contains("active")) {
-        console.log("Clicked outside dropdown; closing dropdown");
-        dropdownMenu.classList.remove("active");
+          dropdownMenu.classList.remove("active");
       }
-    }
   });
 });
